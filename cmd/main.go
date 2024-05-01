4144608ea8b2a6f690a02373a31b288bf8dc4779
Jetta Jacks <jettajac@su-d3.msk.21-school.ru>
2024-05-01 19:55:07 +0300

    Прикручен логгер

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"main/internal/config"
+	"log/slog"
+	"os"
 
 )
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"	
+)
+
 func main() {
-	fmt.Println("Start server")
+	// fmt.Println("Start server")
 	config := config.NewConfig()
-	fmt.Println(config)
+	log:= settupLogger(config.Env)
+	log.Info("Start server", slog.String("env", config.Env))
+	log.Debug("Debug messages")
+}
+
+func settupLogger(env string) *slog.Logger { //!!! возможно перенести  другую папку
+	var log *slog.Logger
+	switch env {
+		case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
